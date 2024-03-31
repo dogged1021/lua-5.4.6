98af5eac2ac53e0775e71b1ef803ff77dabb9cb0
zhang.chen <ro.ai.zhangchen@gmail.com>
2024-03-31 14:18:56 +0800

    init commit

---
+++ build/src/CMakeFiles/lua54.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lapi.c" "src/CMakeFiles/lua54.dir/lapi.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lapi.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lauxlib.c" "src/CMakeFiles/lua54.dir/lauxlib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lauxlib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lbaselib.c" "src/CMakeFiles/lua54.dir/lbaselib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lbaselib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lcode.c" "src/CMakeFiles/lua54.dir/lcode.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lcode.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lcorolib.c" "src/CMakeFiles/lua54.dir/lcorolib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lcorolib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lctype.c" "src/CMakeFiles/lua54.dir/lctype.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lctype.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ldblib.c" "src/CMakeFiles/lua54.dir/ldblib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ldblib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ldebug.c" "src/CMakeFiles/lua54.dir/ldebug.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ldebug.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ldo.c" "src/CMakeFiles/lua54.dir/ldo.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ldo.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ldump.c" "src/CMakeFiles/lua54.dir/ldump.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ldump.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lfunc.c" "src/CMakeFiles/lua54.dir/lfunc.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lfunc.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lgc.c" "src/CMakeFiles/lua54.dir/lgc.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lgc.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/linit.c" "src/CMakeFiles/lua54.dir/linit.c.obj" "msvc" "src/CMakeFiles/lua54.dir/linit.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/liolib.c" "src/CMakeFiles/lua54.dir/liolib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/liolib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/llex.c" "src/CMakeFiles/lua54.dir/llex.c.obj" "msvc" "src/CMakeFiles/lua54.dir/llex.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lmathlib.c" "src/CMakeFiles/lua54.dir/lmathlib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lmathlib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lmem.c" "src/CMakeFiles/lua54.dir/lmem.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lmem.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/loadlib.c" "src/CMakeFiles/lua54.dir/loadlib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/loadlib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lobject.c" "src/CMakeFiles/lua54.dir/lobject.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lobject.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lopcodes.c" "src/CMakeFiles/lua54.dir/lopcodes.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lopcodes.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/loslib.c" "src/CMakeFiles/lua54.dir/loslib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/loslib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lparser.c" "src/CMakeFiles/lua54.dir/lparser.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lparser.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lstate.c" "src/CMakeFiles/lua54.dir/lstate.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lstate.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lstring.c" "src/CMakeFiles/lua54.dir/lstring.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lstring.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lstrlib.c" "src/CMakeFiles/lua54.dir/lstrlib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lstrlib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ltable.c" "src/CMakeFiles/lua54.dir/ltable.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ltable.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ltablib.c" "src/CMakeFiles/lua54.dir/ltablib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ltablib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/ltm.c" "src/CMakeFiles/lua54.dir/ltm.c.obj" "msvc" "src/CMakeFiles/lua54.dir/ltm.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lundump.c" "src/CMakeFiles/lua54.dir/lundump.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lundump.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lutf8lib.c" "src/CMakeFiles/lua54.dir/lutf8lib.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lutf8lib.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lvm.c" "src/CMakeFiles/lua54.dir/lvm.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lvm.c.obj.d"
+  "C:/Users/zhangchen/Desktop/lua-5.4.6/src/lzio.c" "src/CMakeFiles/lua54.dir/lzio.c.obj" "msvc" "src/CMakeFiles/lua54.dir/lzio.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/lua54.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../lua/bin/lua54.lib"
+)
